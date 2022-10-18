585b8627f664169818ae8c7cfbecb8c8a60914c0
bArtOzzzz <breakyourbrain55@gmail.com>
2022-10-18 15:07:54 +0300

    Refactoring

--- AuthenticationMicroservice/Program.cs
+++ AuthenticationMicroservice/Program.cs
@@ -120,9 +120,7 @@ builder.Services.AddApiVersioning(opt =>
     opt.DefaultApiVersion = new ApiVersion(1, 0);
     opt.AssumeDefaultVersionWhenUnspecified = true;
     opt.ReportApiVersions = true;
-    opt.ApiVersionReader = ApiVersionReader.Combine(new UrlSegmentApiVersionReader(),
-                                                    new HeaderApiVersionReader("x-api-version"),
-                                                    new MediaTypeApiVersionReader("x-api-version"));
+    opt.ApiVersionReader = ApiVersionReader.Combine(new MediaTypeApiVersionReader("x-api-version"));
 });
 
 builder.Services.AddVersionedApiExplorer(config =>
--- AuthenticationMicroservice/Validation/UserLoginModelValidator.cs
+++ AuthenticationMicroservice/Validation/UserLoginModelValidator.cs
@@ -9,7 +9,7 @@ namespace AuthenticationMicroservice.Validation
         {
             RuleFor(u => u.Username).Length(3, 22)
                                     .WithMessage("Username should be 3 to 22 characters");
-            
+
             RuleFor(u => u.Password).Length(8, 26)
                                     .WithMessage("Password should be 8 to 26 characters");
         }
